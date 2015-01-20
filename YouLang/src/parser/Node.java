e7f110480896e353930a1b1e925341c312ffa531
Arialaxe <rutherford.niall@gmail.com>
2015-01-20 14:42:12 +0000

    Basic default syntax implementation of the bottom-level parsers (untested...)

--- YouLang/src/parser/Node.java
+++ YouLang/src/parser/Node.java
@@ -8,6 +8,10 @@ public abstract class Node<T> {
 	Grammar parent;
 	String stringToParse;
 	
+	public Node (Grammar newParent) {
+		parent = newParent;
+	}
+	
 	public abstract Parser<T> parser();
 	
 	public void getStringToParse() {
